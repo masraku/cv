ad492b58df414f368a1c0442e2901563b2f58c46
masraku <masendra0303@gmail.com>
2025-01-19 16:56:08 +0700

    ubah nama

--- src/app/page.js
+++ src/app/page.js
@@ -22,11 +22,11 @@ export default function Home() {
             <section className="resume-section" id="about">
                 <div className="resume-section-content">
                     <h1 className="mb-0">
-                        Clarence
-                        <span className="text-primary">Taylor</span>
+                        Laksmana Rakho
+                        <span className="text-primary">Marzendra</span>
                     </h1>
                     <div className="subheading mb-5">
-                        3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
+                        Jalan Inspeksi Kali Sunter RT 08/RW 05 · Kelapa Gading Barat · Jakarta Utara 14240 · (+62) 821 4615 0660 ·
                         <a href="mailto:name@email.com">name@email.com</a>
                     </div>
                     <p className="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
