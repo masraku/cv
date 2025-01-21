7f7f038f8657a09fe5ff89a9688706eb94bcfcf0
masraku <masendra0303@gmail.com>
2025-01-21 20:46:32 +0700

    add bootstrap

--- src/app/page.js
+++ src/app/page.js
@@ -153,31 +153,12 @@ export default function Home() {
                         <li className="list-inline-item"><i className="fab fa-grunt"></i></li>
                         <li className="list-inline-item"><i className="fab fa-npm"></i></li>
                     </ul>
-                    <div className="subheading mb-3">Workflow</div>
-                    <ul className="fa-ul mb-0">
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Mobile-First, Responsive Design
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Cross Browser Testing & Debugging
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Cross Functional Teams
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Agile Development & Scrum
-                        </li>
-                    </ul>
                 </div>
             </section>
             <hr className="m-0" />
 \            <section className="resume-section" id="interests">
                 <div className="resume-section-content">
-                    <h2 className="mb-5">Interests</h2>
+                    <h2 className="mb-5">Portofolio</h2>
                     <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
                     <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
                 </div>
@@ -194,36 +175,6 @@ export default function Home() {
                         <li>
                             <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
                             Mobile Web Specialist - Google Certification
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            2
-                            <sup>nd</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - James Buchanan High School - Hackathon 2006
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-trophy text-warning"></i></span>
-                            3
-                            <sup>rd</sup>
-                            Place - James Buchanan High School - Hackathon 2005
                         </li>
                     </ul>
                 </div>
